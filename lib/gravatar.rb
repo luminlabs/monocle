e64538d6c0eb2ac176c75f26d03fd63c46414912
Matthew Owens <matthewp.owens@gmail.com>
2014-01-31 19:39:46 -0500

    Explanation text on buttons and gravatars from monsters to geometric shapes

--- lib/gravatar.rb
+++ lib/gravatar.rb
@@ -3,6 +3,6 @@ require 'digest/md5'
 module Gravatar extend self
   def url(email)
     hex = Digest::MD5.hexdigest(email.downcase)
-    "https://secure.gravatar.com/avatar/#{hex}/?s=66&d=monsterid"
+    "https://secure.gravatar.com/avatar/#{hex}/?s=66&d=identicon"
   end
 end
