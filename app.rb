132c126f2754d1972b780cacc1e06ac975236849
Matthew Owens <matthewp.owens@gmail.com>
2014-04-19 09:00:49 -0400

    Google analytics addition

--- app.rb
+++ app.rb
@@ -25,6 +25,9 @@ require 'app/extensions'
 require 'app/routes'
 require 'mail'
 
+require 'rack-google-analytics'
+use Rack::GoogleAnalytics, :tracker => 'UA-49704815-1'
+
 module Brisk
   class App < Sinatra::Application
     configure do
